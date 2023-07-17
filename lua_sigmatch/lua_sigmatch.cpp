fca25ecad42d2d85c2cb221327549e214643bf50
Matt Mills <mmills@2bn.net>
2023-07-17 04:29:31 -0600

    Add project files.

---
+++ lua_sigmatch/lua_sigmatch.cpp
@@ -0,0 +1,21 @@
+﻿// lua_sigmatch.cpp : Defines the entry point for the application.
+//
+
+#include "lua_sigmatch.h"
+
+using namespace std;
+
+sigmatch_signature_search(lua_State* L) {
+
+
+
+}
+
+static const struct luaL_reg sigmatchlib[] = {
+	{"signature_search", sigmatch_signature_search},
+	{NULL, NULL}
+};
+
+int luaopen_sigmatch(lua_State* L) {
+
+};
---
+++ lua_sigmatch/lua_sigmatch.h
@@ -0,0 +1,10 @@
+﻿// lua_sigmatch.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include "lib/luajit/src/lua.hpp"
+
+// TODO: Reference additional headers your program requires here.
